05f4aced24f49b576165fb06c9c95fc1f4ef0231
Abrar Burk <burk.abrar@gmail.com>
2020-07-19 22:23:56 +0530

    :sparkles: implement save page and print page

--- src/components/configure-castor-page.js
+++ src/components/configure-castor-page.js
@@ -1,11 +1,15 @@
 
 import React, { Component } from 'react';
+import ReactToPrint, {PrintContextConsumer} from 'react-to-print';
+import htmlToImage from 'html-to-image';
+
 import DropdownMaterial from './formComponents/dropdown-material';
 import DropdownWheelDia from './formComponents/dropdown-wheelDia';
 import InputField from './formComponents/input-field';
 import Radio from './formComponents/radio';
 import Checkbox from './formComponents/checkbox';
 import LineArrow from '../images/lineArrow.svg';
+
 
 var data = require('../data/configure-castor-content.json');
 var productImgPath = require('../data/product-img-path.json');
@@ -80,12 +84,13 @@ class ConfigureCastor extends Component{
       activegrooveOptionsId: activeGrooveOptionsId,
       braking: false,
       quantity: 0,
-      configurable: true
+      configurable: true,
+      showSavingLoader: false
     }
     this.updateWheelConfig = this.updateWheelConfig.bind(this);
     this.updateInput = this.updateInput.bind(this);
     this.validateAndAddToCart = this.validateAndAddToCart.bind(this);
-
+    this.createImgFromHtml = this.createImgFromHtml.bind(this);
 
   }
 
@@ -144,6 +149,17 @@ class ConfigureCastor extends Component{
     this.state.quantity > 0 ? this.props.updateCart(wheelConfig, configurableStatus) : alert("you need to select a quantity to add this item to cart");
   }
 
+  createImgFromHtml(id){
+    this.setState({showSavingLoader: true})
+    htmlToImage.toJpeg(document.getElementById(id), { quality: 0.95 })
+    .then(function (dataUrl) {
+      var link = document.createElement('a');
+      link.download = 'product_dhmwheels.jpeg';
+      link.href = dataUrl;
+      link.click();
+      this.setState({showSavingLoader: false})
+    }.bind(this));
+  }
 
 
 
@@ -173,15 +189,41 @@ class ConfigureCastor extends Component{
 
     let activeVital = vitalsOptions[activevitalsOptionsId] ? vitalsOptions[activevitalsOptionsId] : vitalsOptions[0];
 
+    const toDownloadRef = React.createRef();
+
+    console.log("SHOW!",this.state.showSavingLoader);
+
     return(
 
       <div className="config-castor-wrapper container">
+        { this.state.showSavingLoader &&
+          <div style={{
+              position: 'fixed',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '300px',
+              height:"200px",
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#f4f2ed',
+              zIndex: '10000',
+              border: '1px solid #B5A98C',
+              boxShadow: '1px 1px 7px rgba(0,0,0,0.1)'
+            }}>
+            <h3 style={{ padding: '20px'}} className='black medium'>saving this castor as an image...</h3>
+          </div>  }
         <h4 className="black medium section__title">Build Your Castor</h4>
         <div className="config-panel">
-          <div className="series-bar">
-            <h4 className="black medium"><span className="black regular">{activeDutyName+" Castors"}</span>{activeSeriesName}</h4>
-          </div>
-          <div className="config-panel__body row">
+          <div id="configContainer" style={{backgroundColor: 'white'}} ref={el => (this.componentRef = el)}>
+            <div className="series-bar">
+              <h4 className="black medium">
+                <span className="black regular" style={{marginRight: '8px'}}>{activeDutyName+" Castors"}</span>
+                {activeSeriesName}
+              </h4>
+            </div>
+            <div className="config-panel__body row">
             <div className="image-wrapper col-lg-7 col-md-6 col-sm-6 col-12">
               <img src={process.env.PUBLIC_URL + activeProductImage} alt="" />
             </div>
@@ -272,24 +314,30 @@ class ConfigureCastor extends Component{
               </div>
             </div>
           </div>
+          </div>
           <div className= "configure-panel__cta row">
             <div className="col-lg-7 col-md-6 col-sm-6 col-12 secondary_cta_wrapper">
               <div className="cta_cont">
-                <button class="secondary"><h4 className="black antique">DOWNLOAD TECHNICAL SPECIFICATIONS</h4></button>
+                <button className="secondary" onClick={() => this.createImgFromHtml('configContainer')}><h4 className="black antique">SAVE PAGE</h4></button>
               </div>
-              <div className="cta_cont">
-                <button class="secondary"><h4 className="black antique">SHARE</h4></button>
-              </div>
-              <div className="cta_cont">
-                <button class="secondary"><h4 className="black antique">PRINT PAGE</h4></button>
-              </div>
+              <ReactToPrint content={() => this.componentRef}>
+                <PrintContextConsumer>
+                  {({ handlePrint }) => (
+                    <div className="cta_cont">
+                      <button className="secondary" onClick={handlePrint}>
+                        <h4 className="black antique">PRINT PAGE</h4>
+                      </button>
+                    </div>
+                  )}
+                </PrintContextConsumer>
+              </ReactToPrint>
+              {/*<div className="cta_cont">
+                <button className="secondary"><h4 className="black antique">SHARE</h4></button>
+              </div>*/}
             </div>
             <div className="col-lg-5 col-md-6 col-sm-6 col-12 primary_cta_wrapper">
               <div className="cta_cont">
-                <button class="primary"><h4 className="black antique">REQUEST QUOTE NOW</h4></button>
-              </div>
-              <div className="cta_cont">
-                <button class="primary" onClick={() => this.validateAndAddToCart(this.state, configurableStatus)}><h4 className="black antique">ADD TO QUOTE CART</h4></button>
+                <button className="primary" onClick={() => this.validateAndAddToCart(this.state, configurableStatus)}><h4 className="black antique">ADD TO QUOTE CART</h4></button>
               </div>
             </div>
           </div>
