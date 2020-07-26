d882e6b9172c676ede8b29eb164a681fa0c1a9c7
Abrar Burk <burk.abrar@gmail.com>
2020-07-26 17:33:26 +0530

    improve clickability of hamburger links

--- src/components/headerComponents/hamburger-menu.js
+++ src/components/headerComponents/hamburger-menu.js
@@ -29,32 +29,32 @@ class HamburgerMenu extends Component{
 
         <ul className={hamburgerMenuClass.join(' ')+" hamburger-primaryLinks"}>
         <div className="menu-block">
-          <li className="beige">
+          <li className="ham-link beige">
           Castors:
           </li>
-          <li className={matchId === "light-duty-castors" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "light-duty-castors" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/light-duty-castors">Light Duty</Link>
           </li>
-          <li className={matchId === "medium-duty-castors" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "medium-duty-castors" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/medium-duty-castors">Medium Duty</Link>
           </li>
-          <li className={matchId === "heavy-duty-castors" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "heavy-duty-castors" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/heavy-duty-castors">Heavy Duty</Link>
           </li>
-          <li className={matchId === "extra-heavy-duty-castors" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "extra-heavy-duty-castors" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/extra-heavy-duty-castors">Extra Heavy Duty</Link>
           </li>
           <div class="custom-gap-20"></div>
           <li className="beige">
           Other Products:
           </li>
-          <li className={matchId === "trolleys" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "trolleys" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/trolleys">Trolleys</Link>
           </li>
-          <li className={matchId === "exclusive-products" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "exclusive-products" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/exclusive-products">Exclusive Products</Link>
           </li>
-          <li className={matchId === "spare-wheels" ? 'active' : ''} onClick={this.props.closeHamburger}>
+          <li className={`ham-link ${matchId === "spare-wheels" ? 'active' : ''}`} onClick={this.props.closeHamburger}>
           <Link to="/spare-wheels">Spare Wheels</Link>
           </li>
           </div>
